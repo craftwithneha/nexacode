36b6d10e41f8c0f09c8b28f97b14d2652919ab50
Neha <naiharana173@gmail.com>
2025-09-15 09:45:35 +0500

    initial commit

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,8 @@
 export default function Loading() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-black">
       <div className="flex flex-col items-center space-y-4">
-        <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600"></div>
+        <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-green-700"></div>
         <p className="text-gray-600">Loading...</p>
       </div>
     </div>
